fed567a158ed9c0e054f5c7625bd7d232edb02a1
Pham Minh Tan <tanphamminh2002@gmail.com>
2023-01-21 14:29:50 -0800

    ✅

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	reactStrictMode: true,
+
 	images: {
-		domains: [
-			'avatars.githubusercontent.com',
-			'lh3.googleusercontent.com',
-		],
+		domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
 	},
 };
